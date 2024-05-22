7519d902f96ff22c811031ab01fa5f938c9bb9fd
Don Doricent <107654196+Don-Doricent@users.noreply.github.com>
2024-05-21 20:15:40 -0400

    Add files via upload

--- 100 Devs Follow Along Materials/class13-materials/temperature-converter/js/main.js
+++ 100 Devs Follow Along Materials/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
 //Write your pseduo code first! 
+
+// 0 -> 32
+document.querySelector('#yell').addEventListener('click', convert)
+
+function convert() {
+    // need the value that is celsius
+    let temp = document.querySelector('#sel').value
+    // convert from C to F
+    temp = temp * 9/5 + 32
+    // show
+
+    document.querySelector('#placeToYell').innerText = temp
+}
