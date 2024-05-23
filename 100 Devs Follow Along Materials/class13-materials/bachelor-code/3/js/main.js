2d3b21edc4e72df9b53a955f022c57a797660866
Don Doricent <107654196+Don-Doricent@users.noreply.github.com>
2024-05-22 20:09:13 -0400

    Add files via upload

--- 100 Devs Follow Along Materials/class13-materials/bachelor-code/3/js/main.js
+++ 100 Devs Follow Along Materials/class13-materials/bachelor-code/3/js/main.js
@@ -1,11 +1,44 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+const contestants = document.querySelectorAll('.contestant')
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose ))
 
 function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+	if(click.target.classList.contains('rose')){
+		document.querySelector('#nikki').classList.toggle('hidden')
 	}else{
 		alert("Wrong!");
 	}
 }
+
+// let hello = 'Hello world!';
+
+// let message;
+
+// message = hello;
+
+// alert(hello);
+// alert(message);
+
+// let o = 1;
+// let t = 2;
+
+// alert(o + t)
+
+// let admin;
+// let name;
+
+// name = 'Nikki';
+// admin = name;
+
+// alert(admin);
+
+
+// let ourPlanet;
+
+// ourPlanet = "Earth";
+
+// let currentVisitor;
+// currentVisitor = admin;
+
+// alert('Spoiler Alert, Click:  ' + currentVisitor);
+
---
+++ 100 Devs Follow Along Materials/class13-materials/javascript.info/main.js
@@ -0,0 +1,50 @@
+// let hello = 'Hello world!';
+
+// let message;
+
+// message = hello;
+
+// alert(hello);
+// alert(message);
+
+// let o = 1;
+// let t = 2;
+
+// alert(o + t)
+
+// let admin;
+// let name;
+
+// name = 'Nikki';
+// admin = name;
+
+// alert(admin);
+
+
+// let ourPlanet;
+
+// ourPlanet = "Earth";
+
+// let currentVisitor;
+// currentVisitor = admin;
+
+// alert('Spoiler Alert, Click:  ' + currentVisitor);
+
+function sum(a, b){
+    return a + b;
+}
+
+let result = sum(1,3);
+alert(result);
+
+
+function min(a,b){
+    if (a < b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+let minimum = min(2,10);
+alert(minimum);
