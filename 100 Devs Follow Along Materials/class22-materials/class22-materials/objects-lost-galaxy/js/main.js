242ffe96a9b337398fd6ccca667fb4f672ce02aa
Don Doricent <107654196+Don-Doricent@users.noreply.github.com>
2024-08-28 20:51:26 -0400

    Add files via upload

---
+++ 100 Devs Follow Along Materials/class22-materials/class22-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,22 @@
+//Create a mouse object that has four properties and three methods
+
+let mouse = {}
+
+mouse.brand = 'Logitech'
+mouse.color = 'black'
+mouse.model = 'MX Ergo'
+mouse.wireless = true
+
+
+mouse.leftclick = function(){
+    console.log('LEFT CLICKKKKK')
+}
+
+mouse.rightClick = function(){
+    console.log('RIGHT CLICKKKK')
+}
+
+mouse.scroll = function(){
+    console.log('JUMP JUMP JUMP')
+}
+
