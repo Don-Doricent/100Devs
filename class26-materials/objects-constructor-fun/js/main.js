3d11d2bbb4b43d78eb502b0801332e1480a0ea37
codenamedon <ddoricent97@gmail.com>
2024-09-12 21:11:35 -0400

    API practice.

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,24 @@
-//Create a constructor with 4 properties and 3 methods
+//Create a pizza constructor with 4 properties and 3 methods
+
+function PizzaMachine(size,toppings,crust,sauce){
+    this.size = size
+    this.toppings = toppings
+    this.crust = crust
+    this.sauce = sauce
+
+    this.estimatedDeliveryTime = function(){
+        console.log('Calculating...');
+        
+    }
+    this.burnMouth = function(){
+        console.log('AHHHHHHH!');
+        
+    }
+
+this.frisbee = function(){
+    console.log('YEEEEEEeeeeeeeetttttt');
+    
+}
+}
+
+let pizza = new PizzaMachine('large', ['spinach', 'garlic'], 'deep dish', '')
--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,30 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.size = 'large'
+pizza.toppings = ['spinach', 'onion', 'jalapenos', 'banana peppers', 'garlic']
+pizza.crust = 'stuffed'
+pizza.sauce = 'heavy'
+
+pizza.estimatedDeliveryTime = function(){
+    console.log('Calculating...')
+}
+
+pizza.burnMouth = function(){
+    console.log('AHHHHH!!!')
+}
+
+pizza.frisbee = function(){
+    console.log('YEEEeeeetttt')
+}
+
+// What do you do when you want to add a new pizza? USE A CONSTRUCTOR TO ADD IT
+
+function TypePizza(pizzaSize, pizzaToppings, pizzaCrust, pizzaSauce){
+    this.size = pizzaSize
+    this.toppings = pizzaToppings
+    this.crust = pizzaCrust
+    this.sauce = pizzaSauce
+}
+
