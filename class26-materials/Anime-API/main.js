53655eccb188d4b332bac64c0e89c21b0c447d6e
codenamedon <ddoricent97@gmail.com>
2024-09-14 18:11:53 -0400

    more work added.

--- class26-materials/Anime-API/main.js
+++ class26-materials/Anime-API/main.js
@@ -1,11 +1,20 @@
 // code for the API
 
-fetch("https://api.anidb.net:9001/httpapi?request=anime")
+document.querySelector('button').addEventListener('click', getAnime)
+
+function getAnime(){
+    let anime = document.querySelector('input').value
+
+    fetch(`https://kitsu.io/api/edges=${anime}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data)
+      console.log(data.anime)
+      document.querySelector('h2').innerText = data.anime[0].strDrink
+      document.querySelector('img').src = data.anime[0].strDrinkThumb
+      document.querySelector('h3').innerText = data.anime[0].strInstructions
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
+}
 
