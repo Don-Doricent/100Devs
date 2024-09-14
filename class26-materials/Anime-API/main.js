f2ae4a61411ef740dc57da5e3d752de581f00734
codenamedon <ddoricent97@gmail.com>
2024-09-13 20:35:13 -0400

    working on an anime API webpage

---
+++ class26-materials/Anime-API/main.js
@@ -0,0 +1,11 @@
+// code for the API
+
+fetch("https://api.anidb.net:9001/httpapi?request=anime")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
