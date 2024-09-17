dc4b314d85cee259df0017f89a27747a585a0d43
codenamedon <ddoricent97@gmail.com>
2024-09-16 20:06:41 -0400

    making an anime API website

---
+++ Anime-API/main.js
@@ -0,0 +1,13 @@
+// code for the API
+
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.message)
+      document.querySelector('img').src= data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+
