5dd3e8bc88a8bf43e45b20e330e62097d9894852
codenamedon <ddoricent97@gmail.com>
2025-01-14 17:58:33 -0500

    did this work?

---
+++ 100HourProject/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\DDori\OneDrive\Desktop\100 Devs Follow Along Materials\100HourProject\index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
